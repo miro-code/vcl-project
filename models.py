fix kl prediction head used

models.py
@@ -176,21 +176,23 @@ class BNN(NN):
             loss += nn.functional.cross_entropy(prediction[i], targets)
         return loss
 
-    def KL_loss(self):
+    def KL_loss(self, task_id):
         #For some reason the original code chooses to calculate KL for ALL prediction heads. So all of them are trained towards the prior at which they already are. I will just leave that out for now. 
         loss = 0
         for i in range(self.n_layers - 1):
             loss += torch.sum(KL_of_gaussians(self.weight_means[i], self.weight_variances[i], self.prior_weight_means[i], self.prior_weight_variances[i]))
             loss += torch.sum(KL_of_gaussians(self.bias_means[i], self.bias_variances[i], self.prior_bias_means[i], self.prior_bias_variances[i]))
         
-        loss += torch.sum(KL_of_gaussians(self.weight_last_means[-1], self.weight_last_variances[-1], self.prior_weight_last_means[-1], self.prior_weight_last_variances[-1]))
-        loss += torch.sum(KL_of_gaussians(self.bias_last_means[-1], self.bias_last_variances[-1], self.prior_bias_last_means[-1], self.prior_bias_last_variances[-1]))
+        loss += torch.sum(KL_of_gaussians(self.weight_last_means[task_id], self.weight_last_variances[task_id], self.prior_weight_last_means[task_id], self.prior_weight_last_variances[task_id]))
+        loss += torch.sum(KL_of_gaussians(self.bias_last_means[task_id], self.bias_last_variances[task_id], self.prior_bias_last_means[task_id], self.prior_bias_last_variances[task_id]))
+        #original implementation uses KL for all prediction heads
+        #that will lead to strange behavior for coreset scenario where the model is trained for the next task and the KL term trains there previous tasks prediction head towards its last state
         return loss
     
     def calculate_loss(model, inputs, targets, task_id):
         #compute elboloss and regularize KL divergence by scaling with the training size
         log_likelihood_loss = model.log_likelihood_loss(inputs, targets, task_id)
-        kl_loss = model.KL_loss()
+        kl_loss = model.KL_loss(task_id)
         #TODO
         #return log_likelihood_loss
         return kl_loss / model.training_size - log_likelihood_loss
@@ -275,7 +277,7 @@ class BNN(NN):
         weight_last_variances = []
         bias_last_variances = []
         
-        if(previous_variances is not None and previous_means is not None):
+        if(previous_variances is not None):
         #if the previous model has been  trained with VI already
         #note that when initialising with MLP weights we still use prior 0 for all weights
             for i in range(n_layers - 1):
@@ -284,12 +286,12 @@ class BNN(NN):
                 weight_variances.append(previous_variances[0][i].detach().clone())
                 bias_variances.append(previous_variances[1][i].detach().clone())
 
-                n_previous_tasks = len(previous_means[2])
-                for i in range(n_previous_tasks):
-                    weight_last_means.append(previous_means[2][i].detach().clone())
-                    bias_last_means.append(previous_means[3][i].detach().clone())
-                    weight_last_variances.append(previous_variances[2][i].detach().clone())
-                    bias_last_variances.append(previous_variances[3][i].detach().clone())
+            n_previous_tasks = len(previous_means[2])
+            for i in range(n_previous_tasks):
+                weight_last_means.append(previous_means[2][i].detach().clone())
+                bias_last_means.append(previous_means[3][i].detach().clone())
+                weight_last_variances.append(previous_variances[2][i].detach().clone())
+                bias_last_variances.append(previous_variances[3][i].detach().clone())
         else:
             for i in range(n_layers - 1):
                 weight_means.append(prior_mean)
@@ -297,10 +299,10 @@ class BNN(NN):
                 weight_variances.append(prior_var)
                 bias_variances.append(prior_var)
             
-            weight_last_means.append(prior_mean)
-            bias_last_means.append(prior_mean)
-            weight_last_variances.append(prior_var)
-            bias_last_variances.append(prior_var)
+        weight_last_means.append(prior_mean)
+        bias_last_means.append(prior_mean)
+        weight_last_variances.append(prior_var)
+        bias_last_variances.append(prior_var)
         
         return [weight_means, bias_means, weight_last_means, bias_last_means], [weight_variances, bias_variances, weight_last_variances, bias_last_variances]
 